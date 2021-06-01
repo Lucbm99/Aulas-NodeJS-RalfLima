89e49e508179265f2ac884b47f94d1969a2e2191
Lucas Baradel <lbaradelmarchiori@gmail.com>
2021-05-31 22:08:35 -0300

    Fix - Finalização CRUD Animais

--- pratica-crud-animais/front/app.js
+++ pratica-crud-animais/front/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var expressHandlebars = require('express-handlebars');
-
+var fetch = require('node-fetch');
 
 //App 
 var app = express();
@@ -17,10 +17,83 @@ app.use(express.static(__dirname + '/public'));
 app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+
+
 //Rotas
 app.get('/', function(request, response) {
-    response.render('inicio');
+    fetch('http://localhost:3000/animais', {
+        method: 'GET',
+    })
+    .then(retorno => retorno.json())
+    .then(retorno => response.render('inicio', {
+        dados: retorno
+    }));
+    //vetor dados contem o json
 })
+
+//rota de cadastros - Form
+app.post('/cadastrar', function(request, response) {
+    let nome = request.body.nome_animal;
+    let patas = request.body.patas;
+    let classe = request.body.classe_animal;
+
+
+    let dados = {
+        'nome':nome,
+        'patas':patas,
+        'classe':classe
+    }
+
+    
+    fetch('http://localhost:3000/animais', {
+        method: 'POST',
+        body: JSON.stringify(dados),
+        headers: {'Content-Type':'application/json'}
+    })  
+    .then(response.redirect('/'))
+})
+
+//seleção pelo id -
+app.get('/selecionar/:id', function(request, response) {
+    let id = request.params.id;
+
+    fetch('http://localhost:3000/animais/'+id, {
+        method: 'GET'
+    })
+    .then(retorno => retorno.json())
+    .then(retorno => response.render('selecionar', {dados: retorno}))
+})
+
+app.post('/editar', function(req, res) {
+    let nome = req.body.nome_animal;
+    let patas = req.body.patas;
+    let classe = req.body.classe_animal;
+    let id = req.body.id;
+
+    let dados = {
+        'nome':nome,
+        'patas':patas,
+        'classe':classe
+    }
+
+    fetch('http://localhost:3000/animais/'+id, {
+        method: 'PUT',
+        body: JSON.stringify(dados),
+        headers: {'Content-Type':'application/json'}
+    })
+    .then(res.redirect('/'))
+
+})
+
+
+//rota de deleção
+app.get('/remover/:id', function(request, response) {
+    let id = request.params.id;
+
+    fetch('http://localhost:3000/animais/'+id, {
+        method: 'DELETE',
+    })
+    .then(response.redirect('/'))})
 
 //Servidor
 app.listen(8080);
